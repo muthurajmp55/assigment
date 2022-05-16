24674036833654136e13f896c5ed3ebacf4fd293
muthurajmp55 <36859633+muthurajmp55@users.noreply.github.com>
2022-05-17 01:10:39 +0530

    Add files via upload

---
+++ data/sample_data.py
@@ -0,0 +1,7 @@
+
+
+data = [{"Stock":'TEA',"Type":'Common',"Last_Div":0,"Fixed_Div":None,"Par":100},
+        {"Stock":'POP',"Type":'Common',"Last_Div":0,"Fixed_Div":None,"Par":100},
+        {"Stock":'ALE',"Type":'Common',"Last_Div":0,"Fixed_Div":None,"Par":100},
+        {"Stock":'GIN',"Type":'Common',"Last_Div":0,"Fixed_Div":None,"Par":100},
+        {"Stock":'JOE',"Type":'Common',"Last_Div":0,"Fixed_Div":None,"Par":100}]
